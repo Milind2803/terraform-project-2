3cdcd9eaee720cb0ca0cdd34193a922553b60fee
Milind2803 <95535585+Milind2803@users.noreply.github.com>
2026-04-21 13:09:05 +0530

    zsdf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-ews-baket1zsdsf"
     region         = "us-east-1"
     key            = "DevOps Project-11/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile   = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
